a0fade02fa84409cb753ab1707efce0a0b34bdf0
eben-barnes <eben_barnes@yahoo.com>
2024-11-22 14:04:08 -0500

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,48 @@
+
+<?php
+
+$firstName = "Eben";
+$lastName = "Barnes";
+$age = "41";
+$maritalStatus = "Married";
+$race = "White";
+$favoriteColor = "Blue";
+$shortBio = "Husband, father, manufacturer turned software person.";
+
+
+echo "Name:";
+echo "<br>";
+echo "<br>";
+echo $firstName . "  " . $lastName;
+echo "<br>";
+echo "<br>";
+echo "Age:";
+echo "<br>";
+echo "<br>";
+echo $age;
+echo "<br>";
+echo "<br>";
+echo "Marital Status:";
+echo "<br>";
+echo "<br>";
+echo $maritalStatus;
+echo "<br>";
+echo "<br>";
+echo "Race:";
+echo "<br>";
+echo "<br>";
+echo $race;
+echo "<br>";
+echo "<br>";
+echo "Favorite Color:";
+echo "<br>";
+echo "<br>";
+echo $favoriteColor;
+echo "<br>";
+echo "<br>";
+echo "Short Bio:";
+echo "<br>";
+echo "<br>";
+echo $shortBio;
+
+?>
